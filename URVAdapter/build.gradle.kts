a360fa555fece6c6aaea81a908ba0b165782a9c7
Denis V <denoson@users.noreply.github.com>
2024-09-27 18:38:09 +0300

    add URVItem properties: type, idOwner

--- URVAdapter/build.gradle.kts
+++ URVAdapter/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "denoson"
             artifactId = "URVAdapter"
-            version = "1.0.0"
+            version = "1.0.1"
             afterEvaluate { from(components["release"]) }
         }
     }
