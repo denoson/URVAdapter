256aaebb99d75e0baf42c011eb001ac41b5fec10
Denis V <denoson@users.noreply.github.com>
2024-09-28 18:24:51 +0300

    add urv counter version

--- URVAdapter/build.gradle.kts
+++ URVAdapter/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "denoson"
             artifactId = "URVAdapter"
-            version = "1.0.1"
+            version = "1.0.2"
             afterEvaluate { from(components["release"]) }
         }
     }
