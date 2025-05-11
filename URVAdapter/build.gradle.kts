a0f75754190d20872db72e9d0797c6141d485ea7
Denis V <denoson@users.noreply.github.com>
2025-05-11 16:35:01 +0300

    update version 1.0.9

--- URVAdapter/build.gradle.kts
+++ URVAdapter/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "denoson"
             artifactId = "URVAdapter"
-            version = "1.0.7"
+            version = "1.0.9"
             afterEvaluate { from(components["release"]) }
         }
     }
